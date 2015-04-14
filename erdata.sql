a07ae3ad745d32ef694df4d185ef37cabd6df15a
Sergey Pushkin <pushkinsv@gmail.com>
2015-04-14 07:37:12 +0000

    типизация и именование

--- erdata.sql
+++ erdata.sql
@@ -68,6 +68,12 @@ as $$
 	select array_agg(id) from er.keys where key like _key and domain like coalesce(_domain,'%')
 $$;
 
+create or replace function er.keys(_keys text[], _domain text default null)
+returns int8[] language sql stable
+as $$
+	select array_agg(k.id) from unnest(_keys) as u join er.keys k on k.key like u and domain like coalesce(_domain,'%');
+$$;
+
 create or replace function er.key_new(_key text, _domain text)
 returns int8 language sql
 as $_$
@@ -93,4 +99,75 @@ where r.relkind='r' and nspname not in ('pg_catalog','information_schema')
 and (select array_agg(attname order by attnum) from pg_attribute where attrelid=r.oid and attnum>0 and attnum<4)='{row,e1,r}'::name[]
 group by r.oid, nspname, relname
 ;
+
+-- определения типов и именования
+create or replace function er.typedef(_col text, _keys int8[], _type text, _id int8 default null)
+returns int8 language plpgsql as $_$
+declare
+	id int8:=coalesce(_id,generate_id());
+begin
+	insert into er.data (e1,r,t) select id, er.key('определяющее поле'), _col;
+	insert into er.data (e1,r,t) select id, er.key('определяемый тип'), _type where _type is not null;
+	insert into er.data (e1,r,e2) select unnest, er.key('ключ определения'), id from unnest(_keys);
+	return id;
+end
+$_$;
+
+create or replace function er.typedef(_col text, _key_template text, _type text, _id int8 default null)
+returns int8 language plpgsql as $_$
+declare
+	id int8:=coalesce(_id,generate_id());
+begin
+	insert into er.data (e1,r,t) select id, er.key('определяющее поле'), _col;
+	insert into er.data (e1,r,t) select id, er.key('определяемый тип'), _type where _type is not null;
+	insert into er.data (e1,r,t) select id, er.key('ключ определения'), _key_template;
+	return id;
+end
+$_$;
+
+create or replace function er.namedef(_keys int8[], _id int8 default null)
+returns int8 language plpgsql as $_$
+declare
+	id int8:=coalesce(_id,generate_id());
+begin
+	insert into er.data (e1,r,e2) select unnest, er.key('ключ именования'), id from unnest(_keys);
+	return id;
+end
+$_$;
+
+create or replace function er.namedef(_key_template text, _id int8 default null)
+returns int8 language plpgsql as $_$
+declare
+	id int8:=coalesce(_id,generate_id());
+begin
+	insert into er.data (e1,r,t) select id, er.key('ключ именования'), _key_template;
+	return id;
+end
+$_$;
+
+-- определяющая онтология
+select er.key_new('определяющее поле','metadata');
+select er.key_new('определяемый тип','metadata');
+select er.key_new('ключ определения','metadata');
+select er.key_new('ключ именования','metadata');
+
+select * from er.typedef('e1',er.keys('определяемый тип','metadata'),'определение');
+select * from er.typedef('e1',er.keys('ключ именования','metadata'),'именование');
+
+create view er.typing as
+select k.id as keyid, k.key, k.domain, c.t as column, t.t as type, t.e1 as typedef
+from er.data t
+join er.data c on c.e1=t.e1 and c.r=er.key('определяющее поле','metadata')
+left join er.data ks on ks.r=er.key('ключ определения') and t.e1 in (ks.e1, ks.e2)
+left join er.keys k on k.id=ks.e1 or k.key like ks.t or k.key~ks.t
+where t.r=er.key('определяемый тип')
+;
+
+create view er.naming as
+select k.id as keyid, k.key, k.domain, n.e1 as namedef
+from er.data n
+join er.data ks on ks.r=er.key('ключ именования') and n.e1 in (ks.e1, ks.e2)
+join er.keys k on k.id=ks.e1 or k.key like ks.t or k.key~ks.t
+;
+left join er.data ks on ks.r=er.key('ключ определения') and (ks.e2=t.e1)   (ks.e2=t.e1 and ks.r=er.key('ключ определения')) or (ks.e1=t.e1 and ks.r=er.key('шаблон ключа'))
 
