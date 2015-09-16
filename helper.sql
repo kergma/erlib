66321699c9288045ddb682cb454f8c80c3c41048
Sergey Pushkin <pushkinsv@gmail.com>
2015-09-16 16:48:46 +0700

    prepare for github

--- helper.sql
+++ helper.sql
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 2013-2015 Sergey Pushkin
+https://github.com/kergma/erlib
+
+This file is part of erlib
+
+erlib is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+erlib is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with erlib.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 -- аггрегаты
 
 create or replace function join_aggregate(text, text, text)
